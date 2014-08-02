446c89790ad1223ad21049cd30b1e49996c316e7
danid123 <danid123@users.noreply.github.com>
2014-08-02 10:51:58 -0700

    updating profile controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,9 +1,12 @@
 class ProfileController < ApplicationController
 	before_filter :authenticate_user!
-	def index
-  	get_twets
-	end
 	def create
     @profile = current_user.twets
+  end
+  def index
+  	@profile = Twets.order(updated_at: :desc).limit(25)
+  end
+  def show
+  	@profile = User.find(params[:id]) 
   end
 end
